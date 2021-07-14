9f778520bcdfd8b514a132da8a61d0311b5b1c7d
crazyCodeLove <crazyCodeLove@163.com>
2021-07-15 06:58:52 +0800

    1 add logout

--- stock-info-front/src/views/home/BreadCrumb.vue
+++ stock-info-front/src/views/home/BreadCrumb.vue
@@ -9,14 +9,42 @@
 </template>
 
 <script>
-    // import Cookies from 'js-cookie'
+    import Cookies from 'js-cookie'
+
     export default {
         name: "BreadCrumb",
         methods: {
             logout() {
-                sessionStorage.removeItem("username");
-                // Cookies.remove('token');
-                this.$router.push("/login");
+                let token = Cookies.get('token');
+                this.$http({
+                    method: 'post',
+                    url: '/stock-info/logout',
+                    data: {
+                        param: {
+                            token: token
+                        }
+                    }
+                }).then(res => {
+                    let {result, message} = res.data;
+                    if (!result) {
+                        this.$notify.error({
+                            title: '错误',
+                            message: message
+                        });
+                    } else {
+                        sessionStorage.removeItem("username");
+                        Cookies.remove('token');
+                        this.$notify({
+                            title: '成功',
+                            message: '登出成功',
+                            type: 'success'
+                        });
+                        sessionStorage.removeItem("username");
+                        Cookies.remove('token');
+                        this.$router.push("/login")
+                    }
+
+                })
             }
         }
     }
--- stock-info-front/vue.config.js
+++ stock-info-front/vue.config.js
@@ -12,6 +12,9 @@ let proxyObj = {
 }
 
 module.export = {
+    configureWebpack: {
+        devtool: "source-map"
+    }
     // devServer: {
     //     host: "localhost",
     //     port: 8080,

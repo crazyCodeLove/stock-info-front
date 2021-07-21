d295266e6382dc375c16dfcb42349c79a350944a
crazyCodeLove <crazyCodeLove@163.com>
2021-07-22 05:51:08 +0800

    1 login use sm2 encrypt

--- stock-info-front/src/util/EncryptUtil.js
+++ stock-info-front/src/util/EncryptUtil.js
@@ -12,8 +12,8 @@ export default {
         if (!publicKey) {
             publicKey = sm2PublicKey;
         }
-        if (!cipherMode){
-            cipherMode=1;
+        if (!cipherMode) {
+            cipherMode = 1;
         }
         let encryptData = '04' + smEncrypt.doEncrypt(word, publicKey, cipherMode); // 加密结果
         return encryptData;

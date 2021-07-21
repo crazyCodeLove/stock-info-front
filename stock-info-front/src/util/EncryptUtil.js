8518d00d4799ed9c206cf75c2dfc3e2cf1691957
crazyCodeLove <crazyCodeLove@163.com>
2021-07-21 23:31:33 +0800

    1 add sm2 encrypt

--- stock-info-front/src/util/EncryptUtil.js
+++ stock-info-front/src/util/EncryptUtil.js
@@ -1,8 +1,20 @@
-import cryptoJs from 'crypto-js'
+import cryptoJs from 'crypto-js';
 
+
+let smEncrypt = require('sm-crypto').sm2;
 let encryptKey = '1234567812345678'
+let sm2PublicKey = "04c40e00621656275814fbaf1e88f08c6918a70054966c9f1f893abb9e195a94b799cdad51298fb2375138ee8f40dfdff8064996075bc5cd020c7ccbf1c68985d0"
 
 export default {
+    // smEncrypt 加密
+    sm2Encrypt(word, cipherMode, publicKey) {
+        // cipherMode: 1 - C1C3C2，0 - C1C2C3，默认为1
+        if (!publicKey) {
+            publicKey = sm2PublicKey;
+        }
+        let encryptData = '04' + smEncrypt.doEncrypt(word, publicKey, cipherMode); // 加密结果
+        return encryptData;
+    },
     // 加密函數
     aesEncrypt(word) {
         let key = cryptoJs.enc.Utf8.parse(encryptKey);

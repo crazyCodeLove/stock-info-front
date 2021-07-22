ee176cb0b9d1e0d47ef249c9d83ecf5ab6eec96f
crazyCodeLove <crazyCodeLove@163.com>
2021-07-22 22:00:33 +0800

    1 fetch data function

--- stock-info-front/src/views/platform/FetchData.vue
+++ stock-info-front/src/views/platform/FetchData.vue
@@ -1,15 +1,41 @@
 <template>
     <div>
-        fetch CSI data
+        <div class="main-content">
+            <el-button type="primary" round @click="downloadCsiData">下载 CSI 数据并落库</el-button>
+        </div>
     </div>
 </template>
 
 <script>
     export default {
-        name: "FetchCSIData"
+        name: "FetchCSIData",
+        methods: {
+            downloadCsiData() {
+                this.$http({
+                    method: 'get',
+                    url: '/stock-info/csi/index/download'
+                }).then((res) => {
+                    let {code, result} = res.data;
+                    if (code == 200) {
+                        console.log(result)
+                        this.$notify({
+                            title: '成功',
+                            message: '下载数据成功',
+                            type: 'success'
+                        })
+                    }
+                })
+            }
+        }
     }
 </script>
 
 <style scoped>
-
+    .main-content {
+        margin: 20px 0;
+        border: 2px solid gray;
+        padding: 10px;
+        border-radius: 10px;
+        min-height: 600px;
+    }
 </style>
